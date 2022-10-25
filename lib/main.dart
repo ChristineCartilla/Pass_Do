985af50a0846bf449abc3f6c6d1e6000e2314d93
genocide1599 <61695290+genocide1599@users.noreply.github.com>
2022-10-26 02:33:38 +0800

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:pass_do/constant.dart';
 import 'WelcomeScreen/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Pass&Do',
-      theme: ThemeData(
-        primaryColor: kPrimaryColor,
-        scaffoldBackgroundColor: Colors.blue,
-      ),
-      home: const WelcomeScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Pass&Do',
+        theme: ThemeData(
+          primaryColor: kPrimaryColor,
+          scaffoldBackgroundColor: Colors.blue,
+        ),
+        home: FutureBuilder(
+          future: _fbApp,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            } else if (snapshot.hasData) {
+              return const WelcomeScreen();
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ));
   }
 }
